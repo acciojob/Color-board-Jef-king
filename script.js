0dd75b55cb30475a90761966c4b3c08963d41c4f
Jef-king <147884623+Jef-king@users.noreply.github.com>
2024-04-01 23:23:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,4 +4,10 @@ for (let x = 0; x < 800; x++) {
   let nSq = document.createElement('div');
   nSq.classList.add('square');
   box.appendChild(nSq);
+	nSq.addEventListener("mouseover",(e)=>{
+		e.stopPropagation()
+		setTimeout(()=>{
+			nSq.style.backgoundColor="red";
+		},4000)
+	})
 }
