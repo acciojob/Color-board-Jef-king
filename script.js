6eccb6d3757d2d172232da6e3a90670dea5bc7b8
Jef-king <147884623+Jef-king@users.noreply.github.com>
2024-04-01 19:02:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+let y=document.querySelector(.container);
+for(let x=0;x<800;x++){
+	let i=document.createElement(span);
+	i.setAttribute(class,square);
+	y.appendChild(i);
+}
