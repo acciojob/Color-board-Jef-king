07c8ec57b8cd9ca4b626f5180164a3464efeba7a
Jef-king <147884623+Jef-king@users.noreply.github.com>
2024-04-01 21:40:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let y=document.querySelector(.container);
-for(let x=0;x<800;x++){
-	let i=document.createElement(span);
-	i.setAttribute(class,square);
-	y.appendChild(i);
+let box = document.querySelector(".container");
+
+for (let x = 0; x < 800; x++) {
+  let nSq = document.createElement('div');
+  nSq.className = 'square';
+  box.appendChild(nSq);
 }
